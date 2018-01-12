450de19179eb45bd336042a007457e3b37282de1
German Quinonez <German.Quinonez@fundacion-jala.org>
2018-01-12 17:36:39 -0400

    adding practice1 code

---
+++ GermanQuinonez/practice1.py
@@ -0,0 +1,34 @@
+# Assign values to variables with long names and combining letters with numbers
+# Perform arithmetic operations using all operators
+# Print each results
+
+variable_int_contain_number_123 = 123
+print(variable_int_contain_number_123)
+variable_float_contain_number_456dot0 = 456.0
+print(variable_float_contain_number_456dot0)
+
+first_operand = 23.1
+second_operand = 56
+
+variable_contain_result_for_Sum_operator = first_operand + second_operand
+print(variable_contain_result_for_Sum_operator)
+
+variable_contain_result_for_Subtract_operator = first_operand - second_operand
+print(variable_contain_result_for_Subtract_operator)
+
+variable_contain_result_for_Multiplication_operator = first_operand * second_operand
+print(variable_contain_result_for_Multiplication_operator)
+
+variable_contain_result_for_Division_operator = first_operand / second_operand
+print(variable_contain_result_for_Division_operator)
+
+variable_contain_result_for_Modulus_operator = first_operand % second_operand
+print(variable_contain_result_for_Modulus_operator)
+
+variable_contain_result_for_Exponent_operator = first_operand ** second_operand
+print(variable_contain_result_for_Exponent_operator)
+
+variable_contain_result_for_Floor_Division_operator = first_operand // second_operand
+print(variable_contain_result_for_Floor_Division_operator)
+
+
