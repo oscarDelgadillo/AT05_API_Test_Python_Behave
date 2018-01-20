66cb9d4126fdc2dfeb8f3e02894903af286ff3c4
AbnerMamani <Abner.Mamani@fundacion-jala.org>
2018-01-20 09:37:26 -0400

    practice three exercises

---
+++ AbnerMamani/modulesPractice/circle/perimeter.py
@@ -0,0 +1,8 @@
+#Module that calculates the perimeter
+#importing the Mat class
+from math import pi
+
+# Function that calculates the perimeter of the given circle a radius
+# r parameter that is the radius of the circle
+def perimeter_of_circle(r):
+    return pi * r * 2
